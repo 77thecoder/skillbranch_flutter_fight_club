28de1a6f4ed2ab153dd2644233a3eaff41323c31
DKostin <DKostin@nvbs.ru>
2021-08-14 21:23:44 +0400

    Код с урока

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,62 +1,13 @@
-import 'dart:convert';
-
-import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_fight_club/main.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+import 'lesson_four/test_lesson_four_1.dart';
+import 'lesson_four/test_lesson_four_2.dart';
+import 'lesson_four/test_lesson_four_3.dart';
+import 'lesson_four/test_lesson_four_4.dart';
+
 void main() {
-  testWidgets('module1', (WidgetTester tester) async {
-    await tester.pumpWidget(MyApp());
-
-    final Iterable<Text> allTextsWithGrey = [
-      ...tester.widgetList(find.text("DEFEND")),
-      ...tester.widgetList(find.text("ATTACK")),
-      ...tester.widgetList(find.text("HEAD")),
-      ...tester.widgetList(find.text("TORSO")),
-      ...tester.widgetList(find.text("LEGS")),
-      ...tester.widgetList(find.text("You")),
-      ...tester.widgetList(find.text("Enemy")),
-    ].cast<Text>();
-    final Iterable<Color?> colorsOfTextsWithGrey =
-    allTextsWithGrey.map((e) => e.style?.color).toSet().toList();
-    expect(colorsOfTextsWithGrey.length, 1);
-    expect(colorsOfTextsWithGrey.first, isNotNull);
-
-    expect(
-      colorsOfTextsWithGrey.first,
-      const Color(0xFF161616),
-    );
-
-    final Iterable<Text> allTextsWithWhite = [
-      ...tester.widgetList(find.text("GO")),
-    ].cast<Text>();
-    final List<Color?> colorsOfTextsWithWhite =
-    allTextsWithWhite.map((e) => e.style?.color).toSet().toList();
-    expect(colorsOfTextsWithWhite.length, 1);
-    expect(colorsOfTextsWithWhite.first, isNotNull);
-
-    expect(
-      colorsOfTextsWithWhite.first,
-      isOneOrAnother(const Color(0xDDFFFFFF), const Color(0xDEFFFFFF)),
-    );
-  });
-}
-
-Matcher isOneOrAnother(dynamic one, dynamic another) => OneOrAnotherMatcher(one, another);
-
-class OneOrAnotherMatcher extends Matcher {
-  final dynamic _one;
-  final dynamic _another;
-
-  const OneOrAnotherMatcher(this._one, this._another);
-
-  @override
-  Description describe(Description description) {
-    return description
-        .add('either ${_one.runtimeType}:<$_one> or ${_another.runtimeType}:<$_another>');
-  }
-
-  @override
-  bool matches(Object? item, Map matchState) => item == _one || item == _another;
+  group("l04h01", () => runTestLecture4hometask1());
+  group("l04h02", () => runTestLecture4hometask2());
+  group("l04h03", () => runTestLecture4hometask3());
+  group("l04h04", () => runTestLecture4hometask4());
 }
